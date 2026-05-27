e4a109c744b0a334a648608e95d1e1b9e5c6ee63
gowdaman-dev <gowdamandev@gmail.com>
2026-05-27 10:49:07 +0530

    fix(app): remove duplicate import of UserServiceModule

--- apps/gateway/src/app.module.ts
+++ apps/gateway/src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AuditInterceptor, AuditModule } from '@app/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { UserServiceModule } from './user-service/user-service.module';
-import { UserServiceModule } from './user-service/user-service.module';
 
 @Module({
   imports: [AuditModule, UserServiceModule],
